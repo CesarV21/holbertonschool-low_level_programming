c9aeace77d2ab771dbc346c7a106c19b40d8fab2
CesarV21 <cesarjr1406@icloud.com>
2022-06-14 01:59:06 +0000

    lenguaje c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * 
+ *
+ * 
+ */
+int main() {
+    int intType;
+    char charType;
+
+    // sizeof evaluates the size of a variable
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+    
+    return 0;
+}
