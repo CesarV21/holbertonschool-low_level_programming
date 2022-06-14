4f5d6049ad69e4e7185d93ab2a86242dd902b630
CesarV21 <cesarjr1406@icloud.com>
2022-06-14 02:44:36 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 /**
  * 
- *
+ *that prints the size of various types on the comput
  * 
  */
 int main() {
     int intType;
+    float floatType;
     char charType;
+    long int longtype;
+    long long lolongtype;
+
 
     // sizeof evaluates the size of a variable
     printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
-    
+    printf("Size of char: %zu byte\n", sizeof(longtype;));
+    printf("Size of char: %zu byte\n", sizeof(lolongtype;));
+
     return 0;
-}
+}i
