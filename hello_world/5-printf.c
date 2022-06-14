8e28fa30dcd1362992989eaefb193a699e12b278
CesarV21 <cesarjr1406@icloud.com>
2022-06-14 05:05:48 +0000

    lenguaje c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                                                                                                        
-/**                                                                                                                                                                       
- * main - impresion                                                                                                                                                       
- *                                                                                                                                                                        
- * Return: retorna 0                                                                                                                                                      
- */                                                                                                                                                                       
-int main(void)                                                                                                                                                            
-{printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                                     
-return (0);                                                                                                                                                  
+#include <stdio.h>
+/**
+ * main - impresion
+ *
+ * Return: retorna 0
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
