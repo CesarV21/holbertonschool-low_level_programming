e48eead91e3936880b120b0fded83d1cfcda2442
CesarV21 <cesarjr1406@icloud.com>
2022-06-16 15:46:48 +0000

    lenguaje c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
-#include <stdio.h>                                                                                                                                                        
-/**                                                                                                                                                                       
- *main - Numbers                                                                                                                                                          
- *                                                                                                                                                                        
- * Return: 0                                                                                                                                                              
- */                                                                                                                                                                       
-                                                                                                                                                                          
-int main(void)                                                                                                                                                            
-                                                                                                                                                                          
-{                                                                                                                                                                         
-int n = 0;                                                                                                                                                                
-for (n = 48 ; n <= 57 ; n++)                                                                                                                                              
-putchar (n);                                                                                                                                                              
-putchar ('\n');                                                                                                                                                           
-return (0);                                                                                                                                                               
+#include <stdio.h>
+/**
+ * main - Numbers
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+int n = 0;
+for (n = 48; n <= 57 ; n++)
+putchar (n);
+putchar ('\n');
+return (0);
 }
