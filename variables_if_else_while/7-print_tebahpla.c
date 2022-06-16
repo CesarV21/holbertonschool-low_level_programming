e69f3f963688c9e672abaee64024dec0afe26196
CesarV21 <cesarjr1406@icloud.com>
2022-06-16 16:02:05 +0000

    lenguaje c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,15 @@
-#include <stdio.h>                                                                                                                                                        
-/**                                                                                                                                                                       
- *main - Alphabeth al revez                                                                                                                                               
- *                                                                                                                                                                        
- *Return: 0                                                                                                                                                               
- */                                                                                                                                                                       
-                                                                                                                                                                          
-int main(void)                                                                                                                                                            
-                                                                                                                                                                          
-{                                                                                                                                                                         
-char n;                                                                                                                                                                   
-for (n = 'z' ; n >= 'a' ; n--)                                                                                                                                            
-putchar(n);                                                                                                                                                               
-putchar('\n');                                                                                                                                                            
-return (0);                                                                                                                                                               
-                                                                                                                                                                          
+#include <stdio.h>
+/**
+*main - Alphabeth al revez
+*
+*Return: 0
+*/
+
+int main(void)
+
+{
+char n;
+for (n = 'z' ; n >= 'a' ; n--)
+putchar(n);                                                      
+putchar('\n');                                                                                                      return (0);                                                                                               
 }
