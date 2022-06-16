32ec32598d49aa241bd44566ee87a862a06ee56e
CesarV21 <cesarjr1406@icloud.com>
2022-06-16 03:47:20 +0000

    lenguaje c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always EXIT_SUCCESS
+ *main - print the alphabet
+ *
+ *Return: return zero
  */
 
-int main(void)
-{
+int main(){
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i;
 
-int d = 10;
-
-for (d = 0; d <= 9; d++)
-{
-putchar(d);
+for (i = 0; i < 10; i++){
+putchar(a[i]);
 }
 putchar ('\n');
 return (0);
