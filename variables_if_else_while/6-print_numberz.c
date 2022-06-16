8b495afe64a731fe7a4f8fbd1b6e25a8174972c1
CesarV21 <cesarjr1406@icloud.com>
2022-06-16 02:17:47 +0000

    lenguaje c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always EXIT_SUCCESS
+ */
+
+int main(void)
+{
+
+int d = 10;
+
+for (d = 0; d <= 9;)
+{
+putchar(d);
+d++;
+}
+putchar ('\n');
+return (0);
+}
