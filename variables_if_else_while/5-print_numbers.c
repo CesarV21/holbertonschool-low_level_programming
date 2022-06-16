b2235db4924c9debd7f831957b0637eaacd7140a
CesarV21 <cesarjr1406@icloud.com>
2022-06-16 00:08:31 +0000

    lenguaje c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-	int d = 10;
-	for (d = 0; d <= 10; d++)
-	printf("%ddigit numbers of base 10 starting from 0\n", d);
+int d = 10;
+for (d = 0; d <= 10; d++)
+printf("%d", d);
 return (0);
 }
