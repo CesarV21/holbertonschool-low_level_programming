eb3efa1490c29226c8a96a9383081377775df47b
CesarV21 <cesarjr1406@icloud.com>
2022-06-17 23:50:34 +0000

    lengaje c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - check the code.
+* 
+* Return: Always 
+* @void:print_alphabet_x10
+*/
+
+void print_alphabet_x10(void)
+
+{
+char c;
+
+for (c = 'a'; c <= 'z';)
+{
+_putchar(c*10);
+}
+_putchar('\n');
+}
