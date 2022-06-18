751c21589ecfe51fa823f8915674dd0dd0f7ee06
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 22:02:41 +0000

    lenguaje c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *-bs - the function for returna absoluto
+ *number _absolute: number abolute
+ *Return: return the valor absolute
+ */
+int _abs(int number_absolute)
+{
+if (number_absolute < 0)
+{
+return (number_absolute * -1);
+}
+return (number_absolute);
+}
