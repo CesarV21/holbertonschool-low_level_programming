128c907d6bb6ebfcaf757cbc6b89e5f039530a52
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 06:26:42 +0000

    lenguaje c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
-#include <unistd.h>
 #include "main.h"
 /**
 * print_alphabet - prints
-* 2-main.c
+* Description: print alphabet
 * Return: on success
-* @void: print alphabet tem times
+* @void: print alphabet
 */
+
 void print_alphabet_x10(void);
+
 int main(void)
 {
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-    void print_alphabet(void);
-
+            void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
+	    void print_alphabet(void)
     return (0);
 }
