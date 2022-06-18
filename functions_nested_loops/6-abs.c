28d9380eea0832e92721202140918e6146fd1d38
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 22:18:23 +0000

    lenguaje c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
+ *description found for function _abs
  *this function _abs prints number_absolute
  *@n: this is number_absolute
  *Return: retorna 1 o 0 si no
  */
-
 int _abs(int number_absolute)
 {
 if (number_absolute < 0)
