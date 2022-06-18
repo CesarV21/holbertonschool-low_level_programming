3a11399c3fe4e4c3146d0c5f930c7157c5c6caea
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 22:35:18 +0000

    lenguaje c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-* _abs(int number_absolute)
+*na (int number_absolute)
 *Return: always the number_absolute
 * na: umber_absolute
 */
 int _abs(int na)
 {
+	
 if (na < 0)
 {
 return (na * -1);
