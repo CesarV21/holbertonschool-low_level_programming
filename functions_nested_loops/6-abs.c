3d874e8458faaf01e0edea9321bd7434ce0e8d37
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 22:13:51 +0000

    lenguaje c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- *fuctions _abs
- *@n: this function prints number absolute
- *Return: retorna number absolute
+ *print_sign - print the 1 when is mayus or 0 when is lower
+ *@n: this is variable of alphabet
+ *Return: retorna 1 o 0 si no
  */
+
 int _abs(int number_absolute)
 {
 if (number_absolute < 0)
