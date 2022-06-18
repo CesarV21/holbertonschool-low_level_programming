daf25306eeb0820d12835142c8518eb066748cab
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 23:05:10 +0000

    lenguaje c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - funtion print alphabe
+* Descrption: print
+* Return: On success 1
+* @void: print alphabetx10
+*/
+int _abs(int number_absolute)
+{
+if (number_absolute < 0)
+{
+return (number_absolute * -1);
+}
+return (number_absolute);
+}
