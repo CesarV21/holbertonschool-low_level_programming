06e418c26e30f0d4fc34e2e97e0e23c85006cf96
CesarV21 <cesarjr1406@icloud.com>
2022-06-18 21:11:56 +0000

    lenguaje c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - print the 1 when is mayus 
+ * @c: this is variable of alphabet
+ * Return: retorna 1 o 0
+ */
+int _isalpha(int c)
+{
+if (c <= 'z' && c >= 'a')
+{
+return (1);
+}
+if (c <= 'Z' && c >= 'A')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
