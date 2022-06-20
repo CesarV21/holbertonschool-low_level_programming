bc739ecddac22770468d8bb068bb8036c9209933
CesarV21 <cesarjr1406@icloud.com>
2022-06-20 14:36:28 +0000

    lenguaje C

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,13 @@
  *@it shows the last number
  */
 
-int print_last_digit(int z)
+int print_last_digi()
 {
-int x = z % 10;
-if (z >= 45)
+int z;
+int x;
+z = 0;
+x = z % 10;
+if (x > 45)
 {
 _putchar(x);
 }
