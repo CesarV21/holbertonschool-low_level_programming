f06cd5f1d9bdc14900ca51efa7c977ce3dc30643
CesarV21 <cesarjr1406@icloud.com>
2022-06-20 00:55:41 +0000

    lenguajec

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_last_digit
+ *Descrption: print last digit
+ *Return: On success 1
+ *@it shows the last number
+ */
+
+int print_last_digit(int z)
+{
+int x = z % 10;
+
+if (z >= 45)
+{
+_putchar(x);
+}
+_putchar('\n');
+return (0);
+}
